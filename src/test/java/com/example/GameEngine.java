95edc59aab51e916ff471e97642aa389887cbaee
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-12-22 17:00:50 -0600

    TDD Mockito Integration

--- src/test/java/com/example/GameEngine.java
+++ src/test/java/com/example/GameEngine.java
@@ -2,7 +2,20 @@ package com.example;
 
 public class GameEngine {
 
+    private Player playerOne;
+    private Player playerTwo;
+    private Result lastResult;
+
     private Move lastMove;
+
+    public GameEngine() {
+
+    }
+
+    public GameEngine(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
 
     public void acceptMove (Move move) {
         this.lastMove = move;
@@ -14,6 +27,12 @@ public class GameEngine {
 
     public Result decideWinner(){
         return Result.PLAYER_ONE_WINS;
+    }
+
+    public void playRound(){
+        Move moveOne = playerOne.makeMove();
+        Move moveTwo = playerTwo.makeMove();
+        lastResult = Result.PLAYER_ONE_WINS;
     }
 
 }
--- src/test/java/com/example/GameEngineTest.java
+++ src/test/java/com/example/GameEngineTest.java
@@ -1,10 +1,40 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class GameEngineTest {
+
+    @Mock
+    private Player playerOne;
+
+    @Mock
+    private Player playerTwo;
+
+    private GameEngine gameEngine;
+
+    @BeforeEach
+    void setUp() {
+        gameEngine = new GameEngine(playerOne,playerTwo);
+    }
+
+    @Test
+    void shouldSimulatePlayerMoves(){
+        when(playerOne.makeMove()).thenReturn(Move.ROCK);
+        when(playerTwo.makeMove()).thenReturn(Move.SCISSORS);
+
+        gameEngine.playRound();
+
+        assertEquals(Result.PLAYER_ONE_WINS,gameEngine.decideWinner(),
+                "Player one wins!");
+    }
 
     @Test
     void shouldAcceptPlayerMoves(){
